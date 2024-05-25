f100095e5d155bbede2c95c13a1e5660464f4bd2
Bagros <tom@bagros.eu>
2024-05-25 06:46:13 +0200

    aaaaaaaaaaa

--- api/crud.py
+++ api/crud.py
@@ -2,55 +2,98 @@ from .models import Messages
 from datetime import datetime
 from sqlalchemy.orm import Session
 from sqlalchemy import func
+from sqlalchemy.sql.sqltypes import Integer
 from .schemas import Message, MessageStatus
 
+
 def get_topics(db: Session, topic: str):
-    
-    topics = db.query(Messages.topic).filter(Messages.topic.like(f"{topic}%")).distinct().all()
-    
+
+    topics = db.query(Messages.topic).filter(
+        Messages.topic.like(f"{topic}%")).distinct().all()
+
     if not topics:
         return None
-    
+
     unique_topics = []
-    
+
     for t in topics:
-        c = topic.count('/') #/
+        c = topic.count('/')  # /
         split_topic = t.topic.split("/")[1:]
-        
-        #print(split_topic)
-        #print(c)
-        
+
+        # print(split_topic)
+        # print(c)
+
         if not split_topic[c - 1] in unique_topics:
             unique_topics.append(split_topic[c - 1])
-        
-    return unique_topics    
+
+    return unique_topics
+
 
 def get_messages(db: Session, path: str, start_date: datetime, end_date: datetime):
-    messages = db.query(func.count(Messages.id).label("message_count"),Messages.date.label("date"), func.sum(Messages.size).label('size')).filter(Messages.topic.like(f"{path}%")).filter(Messages.date >= start_date).filter(Messages.date <= end_date).group_by(Messages.date).all()
-    
+    messages = db.query(func.count(Messages.id).label("message_count"), Messages.date.label("date"), func.sum(Messages.size).label('size')).filter(
+        Messages.topic.like(f"{path}%")).filter(Messages.date >= start_date).filter(Messages.date <= end_date).group_by(Messages.date).all()
+
     print(messages)
-    
-    
+
     if len(messages) == 0:
         print("No messages found")
         return None
-    
+
     mess = []
-    
+
     for row in messages:
-        
-        mess.append(Message(count=row.message_count, date=row.date, size=row.size))
-    
+
+        mess.append(Message(count=row.message_count,
+                    date=row.date, size=row.size))
+
     return mess
 
+
+def get_messages_interval(db: Session, path: str, start_date: datetime, end_date: datetime, interval: int):
+    seconds = func.time_to_sec(func.time(Messages.date))
+    rounded_seconds = (seconds / interval).cast(Integer) * interval
+    rounded_time = func.sec_to_time(rounded_seconds)
+
+    messages = db.query(
+        func.count(Messages.id).label("message_count"),
+        func.timestamp(func.date(Messages.date), rounded_time).label("date"),
+        func.sum(Messages.size).label('size')
+    ).filter(
+        Messages.topic.like(f"{path}%")
+    ).filter(
+        Messages.date >= start_date
+    ).filter(
+        Messages.date <= end_date
+    ).group_by(
+        func.date(Messages.date), rounded_time
+    ).all()
+
+    print(messages)
+
+    if len(messages) == 0:
+        print("No messages found")
+        return None
+
+    mess = []
+
+    # Assuming Message is a custom class to structure the results
+    for row in messages:
+        mess.append(Message(count=row.message_count,
+                    date=row.date, size=row.size))
+
+    return mess
+
+
 def get_message_status(db: Session, path: str, start_date: datetime, end_date: datetime):
-    message_count_total = db.query(Messages.id).filter(Messages.topic.like(f"{path}%")).filter(Messages.date >= start_date).filter(Messages.date <= end_date).count()
-    
+    message_count_total = db.query(Messages.id).filter(Messages.topic.like(f"{path}%")).filter(
+        Messages.date >= start_date).filter(Messages.date <= end_date).count()
+
     if not message_count_total:
         return None
-    message_size = db.query(func.sum(Messages.size).label("size")).filter(Messages.topic.like(f"{path}%")).filter(Messages.date >= start_date).filter(Messages.date <= end_date).scalar()
-    
+    message_size = db.query(func.sum(Messages.size).label("size")).filter(Messages.topic.like(
+        f"{path}%")).filter(Messages.date >= start_date).filter(Messages.date <= end_date).scalar()
+
     if not message_size:
         return None
-    
+
     return MessageStatus(count=message_count_total, size=message_size)
